7fc96978437e173540d137e7acd5a5571293f903
omlnaut <oneironaut.oml@gmail.com>
2022-07-24 19:50:44 +0200

    responses wip

--- rentomatic/responses.py
+++ rentomatic/responses.py
@@ -1,6 +1,18 @@
+from dataclasses import dataclass
+
+
 class ResponseSuccess:
     def __init__(self, value):
         self.value = value
 
     def __bool__(self):
         return True
+
+
+class ResponseFailure:
+    def __init__(self, type: str, message: str):
+        self.type = type
+        self.message = message
+
+    def __bool__(self):
+        return False
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,5 +1,23 @@
-from rentomatic.responses import ResponseSuccess
+import pytest
+
+from rentomatic.responses import ResponseSuccess, ResponseFailure
+
+
+@pytest.fixture
+def generic_response_type():
+    return "Response"
+
+
+@pytest.fixture
+def generic_response_message():
+    return "Message"
 
 
 def test_response_success_is_true():
     assert bool(ResponseSuccess(None)) is True
+
+
+def test_response_failure_is_false(generic_response_type, generic_response_message):
+    response = ResponseFailure(generic_response_type, generic_response_message)
+
+    assert bool(response) is False
