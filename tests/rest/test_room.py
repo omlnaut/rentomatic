a78b53c3147e3f07e2f462468799b11981eea076
Oliver Rüger <oliver.rueger@relaxdays.de>
2022-07-25 22:20:00 +0200

    testing against function signatures

--- tests/rest/test_room.py
+++ tests/rest/test_room.py
@@ -4,6 +4,7 @@ from unittest import mock
 import pytest
 
 from rentomatic.domain.room import Room
+from rentomatic.responses import ResponseSuccess
 from ..constants import init_dict, code, serialized_dict
 
 
@@ -12,13 +13,15 @@ def rooms(init_dict):
     return [Room.from_dict(init_dict)]
 
 
-@mock.patch("application.rest.room.room_list_use_case")
+@mock.patch("application.rest.room.room_list_use_case", autospec=True)
 def test_get(mock_use_case, client, serialized_dict, rooms):
-    mock_use_case.return_value = rooms
+    mock_use_case.return_value = ResponseSuccess(rooms)
 
     response = client.get("/rooms")
 
     assert json.loads(response.data.decode("UTF-8")) == [serialized_dict]
     mock_use_case.assert_called()
+    args, kwargs = mock_use_case.call_args
+    assert args[1].filters == {}
     assert response.status_code == 200
     assert response.mimetype == "application/json"
