af04904a7c3d47a7fc826a86d89957fa81ef89c3
Oliver Rüger <oliver.rueger@relaxdays.de>
2022-07-29 07:53:31 +0200

    memrepo filters. continue with cli

--- rentomatic/repository/memrepo.py
+++ rentomatic/repository/memrepo.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Union
 
 from rentomatic.domain.room import Room
 
@@ -7,5 +7,19 @@ class MemRepo:
     def __init__(self, data: list[dict[str, Any]]):
         self.data = data
 
-    def list(self) -> list[Room]:
-        return [Room.from_dict(i) for i in self.data]
+    def list(self, filters: dict[str, Union[str, int]]) -> list[Room]:
+        rooms = [Room.from_dict(i) for i in self.data]
+
+        if "code__eq" in filters:
+            rooms = [room for room in rooms if room.code == filters["code__eq"]]
+
+        if "price__eq" in filters:
+            rooms = [room for room in rooms if room.price == int(filters["price__eq"])]
+
+        if "price__lt" in filters:
+            rooms = [room for room in rooms if room.price < int(filters["price__lt"])]
+
+        if "price__gt" in filters:
+            rooms = [room for room in rooms if room.price > int(filters["price__gt"])]
+
+        return rooms
